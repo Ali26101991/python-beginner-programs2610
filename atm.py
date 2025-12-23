6ea30b1b5b01c87d41ce0d43638879fce8db8bae
VotaryTech <votarytech@VOTARYTECH.COM>
2025-12-23 17:51:40 +0530

    Add PIN validation to ATM program

--- atm.py
+++ atm.py
@@ -1,9 +1,14 @@
-balance=1000
-withdraw=int(input("enter required amount :"))
-if withdraw<=balance:
-    balance -= withdraw
-    print("withdrawl is successfull")
-    print("Remaining balance is ",balance)
+pin = input("Enter PIN: ")
 
+if pin == "1234":
+    balance = 1000
+    withdraw = int(input("Enter required amount: "))
+
+    if withdraw <= balance:
+        balance -= withdraw
+        print("Withdrawal successful")
+        print("Remaining balance:", balance)
+    else:
+        print("Insufficient balance")
 else:
-    print("insufficient balance")
+    print("Invalid PIN")
